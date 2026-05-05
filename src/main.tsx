6d6887c7652bbf10ddfb052e4a7d094f8cd9340a
Carlos Ortiz G <carlos.eog97@gmail.com>
2026-05-04 22:04:31 -0500

    Update index.html language attribute to Spanish, enhance CSS variables for dark and light themes, and add new components for sidebar navigation and interactive charts. Introduce new data structure for dashboard and integrate drag-and-drop functionality in data tables.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { TooltipProvider } from './components/ui/tooltip.tsx'
 import './index.css'
 import router from './app/router.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <TooltipProvider> 
     <RouterProvider router={router} />
+    </TooltipProvider>
   </StrictMode>,
 )
--- src/shared/components/MainLayout.tsx
+++ src/shared/components/MainLayout.tsx
@@ -1,9 +1,39 @@
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+import { DataTable } from "@/components/data-table"
+import data from "@/app/dashboard/data.json"
+import { SectionCards } from "@/components/section-cards"
+import { ChartAreaInteractive } from "@/components/chart-area-interactive"
+import { SiteHeader } from "@/components/site-header"
+
 
 export default function MainLayout() {
     return (
         <>
-            <div className="[--header-height:calc(--spacing(14))]">
-            </div>
+            <SidebarProvider
+                style={
+                    {
+                        "--sidebar-width": "calc(var(--spacing) * 72)",
+                        "--header-height": "calc(var(--spacing) * 12)",
+                    } as React.CSSProperties
+                }
+            >
+                <AppSidebar variant="inset" />
+                <SidebarInset>
+                    <SiteHeader />
+                    <div className="flex flex-1 flex-col">
+                        <div className="@container/main flex flex-1 flex-col gap-2">
+                            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                                <SectionCards />
+                                <div className="px-4 lg:px-6">
+                                    <ChartAreaInteractive />
+                                </div>
+                                <DataTable data={data} />
+                            </div>
+                        </div>
+                    </div>
+                </SidebarInset>
+            </SidebarProvider>
         </>
     )
 }
